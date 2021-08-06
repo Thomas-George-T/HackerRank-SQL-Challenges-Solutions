3fd2e567e1121281c055b53b6521f4521bbdcd8b
Thomas George Thomas <tgt555@gmail.com>
2021-08-06 17:45:45 +0530

    Update Weather Observation Station 20.sql

--- Aggregation/Weather Observation Station 20.sql
+++ Aggregation/Weather Observation Station 20.sql
@@ -1,7 +1,3 @@
-SELECT round(A.LAT_N,4) as median_LAT_N
-FROM (
-SELECT S.LAT_N, @rownum:=@rownum+1 as row_number, @total_rows:=@rownum
-  FROM STATION S, (SELECT @rownum:=0) r
-  ORDER BY S.LAT_N
-) as A
-WHERE A.row_number IN ( FLOOR((@total_rows+1)/2), FLOOR((@total_rows+2)/2) );
+SELECT Round(st.lat_n, 4)
+FROM station AS st
+WHERE (SELECT Count(lat_n) FROM station WHERE lat_n < st.lat_n) = (SELECT Count(lat_n) FROM station WHERE lat_n > st.lat_n);
