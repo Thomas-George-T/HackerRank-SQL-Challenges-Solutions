7660660ab587c8ae4d354aa54dc6a11c48f6f3b7
Thomas George Thomas <tgt555@gmail.com>
2022-04-15 18:41:40 -0400

    Create Contest Leaderboard.sql

---
+++ Basic Join/Contest Leaderboard.sql
@@ -0,0 +1,11 @@
+SELECT h.hacker_id, h.name, SUM(MAX_SCORE.t1) as total_score
+FROM Hackers h inner join 
+(
+    SELECT MAX(s.score) as t1, s.hacker_id  
+    FROM Submissions s
+    GROUP BY s.challenge_id, s.hacker_id
+) AS MAX_SCORE
+ON h.hacker_id = MAX_SCORE.hacker_id
+GROUP BY h.hacker_id, h.name
+HAVING total_score > 0 
+ORDER BY total_score DESC, hacker_id ASC
