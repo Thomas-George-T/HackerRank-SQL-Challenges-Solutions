35796007e64117d97403d7a11e09e219bb84f3ff
Ishan <68941034+StackDSA@users.noreply.github.com>
2022-08-02 17:59:06 +0530

    Update Type of Triangle.sql

--- Advanced Select/Type of Triangle.sql
+++ Advanced Select/Type of Triangle.sql
@@ -5,5 +5,6 @@ when A + B <= C or A + C <= B or B + C <= A then "Not A Triangle"
 when A = B and B = C then "Equilateral"
 when A = B or A = C or B = C then "Isosceles"
 else "Scalene"
-end as triangle_sides
+end as triangle_sides 
+#In the question we don't have to give a column name to the type of triangle. Thus, we can just write END instead of END AS "Triangle_sides". 
 from TRIANGLES
