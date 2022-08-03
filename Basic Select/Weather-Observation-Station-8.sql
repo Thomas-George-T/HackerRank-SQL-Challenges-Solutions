41988b81df2edaa42a19448b41df718ead5e0891
Ishan <68941034+StackDSA@users.noreply.github.com>
2022-08-03 17:54:08 +0530

    Update Weather-Observation-Station-8.sql

--- Basic Select/Weather-Observation-Station-8.sql
+++ Basic Select/Weather-Observation-Station-8.sql
@@ -1,4 +1,2 @@
 select distinct city from station where left(city,1) in('a','e','i','o','u') and right(city,1) in('a','e','i','o','u') 
-# https://www.geeksforgeeks.org/left-function-in-mysql/
-# https://www.w3schools.com/sql/func_mysql_right.asp
-# Author: Ishan Deshpande
+
