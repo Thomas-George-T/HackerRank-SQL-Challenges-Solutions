147cebfed57dfd179e864636ca09b915d17c411d
Ishan <68941034+StackDSA@users.noreply.github.com>
2022-08-03 18:08:42 +0530

    Update Weather-Observation-Station-10.sql

--- Basic Select/Weather-Observation-Station-10.sql
+++ Basic Select/Weather-Observation-Station-10.sql
@@ -1,9 +1,9 @@
-# Author: Ishan Deshpande
+# Author: Thomas George Thomas
 select distinct(city) from station where
 city not like "%a" and
 city not like "%e" and
 city not like "%i" and
 city not like "%o" and
 city not like "%u";
-# SHORTER SOLUTION:
+# SHORTER SOLUTION: 
 SELECT DISTINCT(CITY) from STATION WHERE RIGHT(CITY,1) NOT IN('a','e','i','o','u')
