c508c2bd2722a5d512176aa6829690867e8f4aa9
Ishan <68941034+StackDSA@users.noreply.github.com>
2022-08-03 17:50:17 +0530

    Update Average Population of Each Continent.sql

--- Basic Join/Average Population of Each Continent.sql
+++ Basic Join/Average Population of Each Continent.sql
@@ -1,3 +1,1 @@
 select country.continent, floor(avg(city.population)) from country join city on city.countrycode = country.code group by country.continent;
-#ALTERNATE SOLUTION:
-select country.continent, floor(avg(city.population)) from city,COUNTRY WHERE city.countrycode = country.code group by country.continent;
