223c36db4659a31bd0fd14fd9c64907c9e774112
Thomas George Thomas <tgt555@gmail.com>
2022-08-15 00:00:44 -0400

    Revert "Added Draw the Triangle questions under Advanced SELECT. Provided alternate solution for Weather Observation Station 10. Reverted the changes suggested in previous Pull request."

--- Basic Select/Weather-Observation-Station-10.sql
+++ Basic Select/Weather-Observation-Station-10.sql
@@ -5,5 +5,4 @@ city not like "%e" and
 city not like "%i" and
 city not like "%o" and
 city not like "%u";
--- SHORTER SOLUTION: 
-SELECT DISTINCT(CITY) from STATION WHERE RIGHT(CITY,1) NOT IN('a','e','i','o','u')
+
