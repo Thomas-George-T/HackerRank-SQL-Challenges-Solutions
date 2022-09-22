82b5f8a7869c37424489744bcc379eecc0db2397
nottherightlynx <114174670+nottherightlynx@users.noreply.github.com>
2022-09-22 11:10:34 -0600

    Update Weather-Observation-Station-12.sql
    function error, left/right incorrect syntax and function
    
    https://www.w3schools.com/sql/func_sqlserver_left.asp

--- Basic Select/Weather-Observation-Station-12.sql
+++ Basic Select/Weather-Observation-Station-12.sql
@@ -1,1 +1,11 @@
 select distinct city from station where left(city,1) not in('a','e','i','o','u') and right(city,1) not in('a','e','i','o','u');
+
+# that left() and right() stuff is wrong, use substr()
+
+SELECT DISTINCT(city) 
+FROM STATION 
+WHERE 
+  LOWER(SUBSTR(CITY,1,1)) 
+  NOT IN ('a','e','i','o','u') 
+  AND LOWER(SUBSTR(CITY,LENGTH(CITY),1)) 
+  NOT IN ('a','e','i','o','u');
