b5cd22802921b1132a3c7cc1834ab4c5d0d263c4
Ibrahim Giwa <76513466+Ibrahim-netizen@users.noreply.github.com>
2023-05-17 16:18:13 +0100

    Update Weather-Observation-Station-10.sql
    Suited for mysql and Postgres DBs.

--- Basic Select/Weather-Observation-Station-10.sql
+++ Basic Select/Weather-Observation-Station-10.sql
@@ -1,8 +1,4 @@
-# Author: Thomas George Thomas
-select distinct(city) from station where
-city not like "%a" and
-city not like "%e" and
-city not like "%i" and
-city not like "%o" and
-city not like "%u";
+# Author: Ibrahim Giwa
+
+Select distinct city from station where city not regexp '[aeiou]$';
 
