4bda5f113eb33bd718120e566c4727927643c059
Ibrahim-netizen <giwaibrahim05@gmail.com>
2023-09-27 23:55:52 +0100

    To retrieve different city names that does not end with vowels in PostgreSQL DB

--- Basic Select/Weather-Observation-Station-10.sql
+++ Basic Select/Weather-Observation-Station-10.sql
@@ -1,8 +1,3 @@
-# Author: Thomas George Thomas
-select distinct(city) from station where
-city not like "%a" and
-city not like "%e" and
-city not like "%i" and
-city not like "%o" and
-city not like "%u";
+# Author: Ibrahim Giwa
+select distinct city from station where city not like '%[aeiou]'
 
